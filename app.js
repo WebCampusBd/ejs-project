aa35dbc230b3b0f24711acabd1caa3c18c288d95
Ratul Anjum <mdratulanjum@gmail.com>
2024-03-10 18:30:00 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({extended : false}));
+
+let pLanguages = [];
+
+app.get("/", (req,res)=>{
+    res.statusCode = 200;
+    res.render("home", {pLanguages});
+});
+
+app.post("/register", (req,res)=>{
+    const pLanguage = req.body.pLanguage;
+    pLanguages.push(pLanguage);
+    res.redirect("/");
+});
+
+app.get("/contact", (req,res)=>{
+    res.statusCode = 200;
+    res.render("contact", {});
+});
+
+app.use((req,res,next)=>{
+    res.statusCode = 404;
+    res.send("<h1>404 Not Found</h1>");
+});
+
+app.use((err,req,res,next)=>{
+    res.statusCode = 500;
+    res.send({
+        message : "Something Broke!"
+    })
+});
+
+exports.app = app;
+
